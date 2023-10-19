cf1c527226f8124793d94dd6d117472610781b08
Dongmin <pdm001125@naver.com>
2023-10-19 19:27:01 +0900

    feat: baseActivity 구현

---
+++ app/src/main/java/org/sopt/dosopttemplate/BaseActivity.kt
@@ -0,0 +1,39 @@
+package org.sopt.dosopttemplate
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<T : ViewBinding>(
+    val bindingFactory: (LayoutInflater) -> T
+) : AppCompatActivity() {
+    private var _binding: T? = null
+    val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingFactory(layoutInflater)
+        setContentView(binding.root)
+
+        hideKeyboards()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    fun hideKeyboards() {
+        binding.root.setOnClickListener { view ->
+            // 키보드 내리기
+            val controller = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            controller.hideSoftInputFromWindow(view.windowToken, 0)
+
+            // 포커스 없애기
+            view.clearFocus()
+        }
+    }
+}
