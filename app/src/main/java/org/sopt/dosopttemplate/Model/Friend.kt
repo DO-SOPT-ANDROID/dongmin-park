773eb307e802f59d0387cd894a8d8f5cbd354146
Dongmin <pdm001125@naver.com>
2023-10-24 21:16:23 +0900

    refactor: 미사용 클래스 삭제

--- app/src/main/java/org/sopt/dosopttemplate/Model/Friend.kt
+++
@@ -1,9 +0,0 @@
-package org.sopt.dosopttemplate.Model
-
-import androidx.annotation.DrawableRes
-
-data class Friend(
-    @DrawableRes val profileImage: Int,
-    val name: String,
-    val aboutMe: String,
-)
