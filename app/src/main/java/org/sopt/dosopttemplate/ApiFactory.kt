fb930c519fd5df7b6ace1991af4e6b944c95d392
Dongmin <pdm001125@naver.com>
2023-11-14 13:25:11 +0900

    feat: LoggingInterceptor 셋팅

--- app/src/main/java/org/sopt/dosopttemplate/ApiFactory.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ApiFactory.kt
@@ -1,16 +1,34 @@
 package org.sopt.dosopttemplate
 
+import android.util.Log
+
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object ApiFactory {
     private const val BASE_URL = BuildConfig.AUTH_BASE_URL
 
+    private fun getLogOkHttpClient(): Interceptor {
+        val loggingInterceptor = HttpLoggingInterceptor { message ->
+            Log.d("Retrofit2", "CONNECTION INFO -> $message")
+        }
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(getLogOkHttpClient())
+        .build()
+
     val retrofit: Retrofit by lazy{
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
