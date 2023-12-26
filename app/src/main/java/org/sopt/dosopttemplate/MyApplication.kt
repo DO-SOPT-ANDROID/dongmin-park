421d6eaef016447d44de7d8741d2b22bebe5c1e4
Dongmin <pdm001125@naver.com>
2023-12-26 21:59:52 +0900

    feat: Hilt 종속성 추가

---
+++ app/src/main/java/org/sopt/dosopttemplate/MyApplication.kt
@@ -0,0 +1,14 @@
+package org.sopt.dosopttemplate
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
